3d2c02089a39aef63ea2c648626333833575df9d
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-05-20 11:40:20 +0530

    test

--- karma.conf.js
+++ karma.conf.js
@@ -25,11 +25,11 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require("path").join(__dirname, "./coverage/angular-unit-testing"),
+      dir: require("path").join(__dirname, "./coverage/"),
       subdir: ".",
       reporters: [
-        { type: "html" },
-        { type: "text-summary" },
+        // { type: "html" },
+        // { type: "text-summary" },
         { type: "lcov", subdir: "report-lcov" },
       ],
     },
