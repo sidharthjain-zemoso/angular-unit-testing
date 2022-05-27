79b4c9683b96d67c5cdeb523541e38cb290083a9
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-05-27 12:05:44 +0530

    fixed linter errors

--- src/app/shared/reverse.pipe.ts
+++ src/app/shared/reverse.pipe.ts
@@ -1,9 +1,9 @@
-import { Pipe } from "@angular/core";
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
   name: 'reverse'
 })
-export class ReversePipe {
+export class ReversePipe implements PipeTransform {
   transform(value: string) {
     return value.split("").reverse().join("");
   }
